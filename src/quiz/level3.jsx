a1240dda4eafbccf62d231ef1df43973ea6ed077
Yury Tatsenka <forthepost@gmail.com>
2021-10-01 14:56:21 +0300

    initial commit

--- src/quiz/level3.jsx
+++ src/quiz/level3.jsx
@@ -11,10 +11,21 @@ export const QuizLevel3 = ({
 }) => {
   // Create other refs here
   const nameRef = useRef();
+  const phoneRef = useRef();
+  const cookingRef = useRef();
+  const washingRef = useRef();
+  const stylingRef = useRef();
+  const programmingRef = useRef();
 
   // Add some code inside this function for sending data from the form to the document
   const applyFormData = (e) => {
     e.preventDefault();
+    sendName(nameRef.current.value);
+    sendPhone("+375" + phoneRef.current.value);
+    sendCookingDone(cookingRef.current.checked);
+    sendWashingDone(washingRef.current.checked);
+    sendStylingDone(stylingRef.current.checked);
+    sendProgrammingDone(programmingRef.current.checked);
   };
 
   return (
@@ -24,24 +35,24 @@ export const QuizLevel3 = ({
       <input id="name-input" placeholder="Teddy Brown" ref={nameRef} />
 
       <label htmlFor="phone-input">Phone:</label>
-      <input id="phone-input" placeholder="44 123 45 67" />
+      <input id="phone-input" placeholder="44 123 45 67" ref={phoneRef} />
 
       <label htmlFor="activities-input">Done:</label>
 
       <div id="activities-input">
-        <input type="checkbox" id="cooking" />
+        <input type="checkbox" id="cooking" ref={cookingRef} />
         <label htmlFor="cooking">Cooked 3 burgers</label>
         <br />
 
-        <input type="checkbox" id="washing" />
+        <input type="checkbox" id="washing" ref={washingRef} />
         <label htmlFor="washing">Washed a car</label>
         <br />
 
-        <input type="checkbox" id="styling" />
+        <input type="checkbox" id="styling" ref={stylingRef} />
         <label htmlFor="styling">Styled a room</label>
         <br />
 
-        <input type="checkbox" id="programming" />
+        <input type="checkbox" id="programming" ref={programmingRef} />
         <label htmlFor="programming">Programmed a form</label>
         <br />
       </div>
